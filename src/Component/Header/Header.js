f63bd5d9e24d0652c73d348fcf3263b4086bdaa1
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-05-05 01:19:24 +0600

    home start

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
+                        <div className="navbar-nav ms-auto">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             <Link className="nav-link" to="/about">About</Link>
                             <Link className="nav-link" to="/blog">Blog</Link>
