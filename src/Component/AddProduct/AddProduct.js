1cc1a093aa49c5651c22f2bb6d451e036a1e742e
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-05-07 17:51:53 +0600

    edit css on product

--- src/Component/AddProduct/AddProduct.js
+++ src/Component/AddProduct/AddProduct.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AddProduct.css'
 import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
@@ -21,17 +22,17 @@ const AddProduct = () => {
 
     };
     return (
-        <div>
-            <h1>Add Product</h1>
+        <div className='add-product p-3'>
+            <h1 className='p-3'>Add Product</h1>
             <form className='d-flex flex-column w-50 m-auto p-5' onSubmit={handleSubmit(onSubmit)}>
 
-                <input className='mb-1' placeholder='name' {...register("name", { required: true })} />
-                <textarea className='mb-1' placeholder='description' {...register("description")} />
-                <input className='mb-1' placeholder='supplier' {...register("supplier")} />
-                <input className='mb-1' placeholder='price' type="number" {...register("price")} />
-                <input className='mb-1' placeholder='quantity' type="number" {...register("quantity")} />
-                <input className='mb-1' placeholder='image Url' {...register("image")} />
-                <input type="submit" value='Update' />
+                <input className='mb-3 input' placeholder='name' {...register("name", { required: true })} />
+                <textarea className='mb-3 input' placeholder='description' {...register("description")} />
+                <input className='mb-3 input' placeholder='quantity' type="number" {...register("quantity")} />
+                <input className='mb-3 input' placeholder='price' type="number" {...register("price")} />
+                <input className='mb-3 input' placeholder='supplier' {...register("supplier")} />
+                <input className='mb-3 input' placeholder='image Url' {...register("image")} />
+                <input className='btn btn-success input' type="submit" value='Update' />
             </form>
         </div>
     );
