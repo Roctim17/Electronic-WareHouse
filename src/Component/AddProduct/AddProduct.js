499a9c1719e93ce1d89dd8c23246c97a796cad05
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-05-07 16:16:27 +0600

    done update

--- src/Component/AddProduct/AddProduct.js
+++ src/Component/AddProduct/AddProduct.js
@@ -3,15 +3,35 @@ import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+        const url = `http://localhost:5000/product`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+
+    };
     return (
         <div>
             <h1>Add Product</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form className='d-flex flex-column w-50 m-auto p-5' onSubmit={handleSubmit(onSubmit)}>
 
-                <input defaultValue="test" {...register("example")} />
-                <input {...register("exampleRequired", { required: true })} />
-                <input type="submit" />
+                <input className='mb-1' placeholder='name' {...register("name", { required: true })} />
+                <textarea className='mb-1' placeholder='description' {...register("description")} />
+                <input className='mb-1' placeholder='supplier' {...register("supplier")} />
+                <input className='mb-1' placeholder='price' type="number" {...register("price")} />
+                <input className='mb-1' placeholder='quantity' type="number" {...register("quantity")} />
+                <input className='mb-1' placeholder='image Url' {...register("image")} />
+                <input type="submit" value='Update' />
             </form>
         </div>
     );
